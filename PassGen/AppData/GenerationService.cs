ffb40573f20a373d1d30c4bc05f0038cdb6d9825
Гоголев.Валерий <.@TERM1>
2025-01-23 16:30:38 +0300

    Что-то делали

--- PassGen/AppData/GenerationService.cs
+++ PassGen/AppData/GenerationService.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PassGen.AppData
 {
     internal class GenerationService
     {
+        private readonly Random _random = new Random();
+        private readonly string _numbers = "1234567890";
+        private readonly string _symbols = "!@#$%^&*()_-=";
+        private readonly string _loverCharacters = "qwertyuiopasdfghjklzxcvbnm";
+        private readonly string _upperCharacters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+
+
+        public List<string> Start(int length)
+        {
+
+
+            string[] patterns = new string[] { _numbers, _symbols, _loverCharacters, _upperCharacters };
+            string password = string.Empty;
+            var passwordSets = new List<string>();
+
+            while (password.Length < length)
+            {
+                int patternIndex = _random.Next(0, patterns.Length);
+                int charIndexFromPattern = _random.Next(0, patterns[patternIndex].Length);
+
+                password += patterns[patternIndex][charIndexFromPattern];
+            }
+
+            return passwordSets;
+
+        }
     }
 }
+
+
+
+
+
+
--- PassGen/Viev/Windows/MainWindow.xaml.cs
+++ PassGen/Viev/Windows/MainWindow.xaml.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using PassGen.AppData;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace PassGen.Viev.Windows
 {
@@ -19,9 +8,18 @@ namespace PassGen.Viev.Windows
     /// </summary>
     public partial class MainWindow : Window
     {
+        GenerationService _generationService;
         public MainWindow()
         {
             InitializeComponent();
+
+            _generationService = new GenerationService();
+        }
+
+        private void GenerateBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int length = int.Parse(PasswordLengthTb.Text);
+            PasswordsLb.ItemsSource = _generationService.Start(length);
         }
     }
 }
